e69418990f551e079d62a512449adadeb1cf5db8
Vijayesvar <vijayesvar2005@gmail.com>
2025-06-10 16:36:43 +0530

    Update for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +11,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // For GitHub Pages
+  basePath: isGithubActions ? '/Pledg' : '',
+  assetPrefix: isGithubActions ? '/Pledg/' : '',
+  output: 'export',
+  distDir: 'out',
+  trailingSlash: true,
 }
 
 export default nextConfig
