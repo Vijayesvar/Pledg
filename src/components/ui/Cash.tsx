ed132a4bb87324eaaeefe1a6f0b0c8f4b686a5c2
mukesh <mukeshramasubramanian@gmail.com>
2025-07-17 20:45:54 +0530

    fix: test commit for cash misallignment - 3

--- src/components/ui/Cash.tsx
+++ src/components/ui/Cash.tsx
@@ -5,8 +5,8 @@ export default function Coins() {
     <Image
       src="/cash.png"
       alt="Cash"
-      width={1200}
-      height={1200}
+      width={1000}
+      height={1000}
       className="lg:w-full lg:h-full md:w-[0%] md:h-[0%]"
       priority
     />
